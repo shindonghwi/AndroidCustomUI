2d8920523408ec088d4f9a81b8a9b79e4cecd653
shindonghwi <ehdgnl8940@naver.com>
2022-07-13 16:31:34 +0900

    feat: 컴포넌트 커스텀뷰 탭 생성

--- app/src/main/java/com/example/androidcustomui/navigation/nav_graph/CompanyNavGraph.kt
+++ app/src/main/java/com/example/androidcustomui/navigation/nav_graph/CompanyNavGraph.kt
@@ -5,17 +5,18 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import com.example.androidcustomui.model.screen_router.CompanyScreenRouter
+import com.example.androidcustomui.ui.screen.TabCustomView
 
 fun NavGraphBuilder.addCompanyGraph(
     modifier: Modifier = Modifier,
     navController: NavController
 ) {
     composable(CompanyScreenRouter.ROOT.route) {
-//        CompanyScreen {
-//            navController.navigate(CompanyScreenRouter.DETAIL.route)
-//        }
+        TabCustomView {
+            navController.navigate(CompanyScreenRouter.DETAIL.route)
+        }
     }
-    composable(CompanyScreenRouter.DETAIL.route) {
+//    composable(CompanyScreenRouter.DETAIL.route) {
 //        DetailScreen(from = CompanyScreenRouter.DETAIL.route) {
 //            Button(onClick = {
 //                navController.navigate(CompanyScreenRouter.DETAIL2.route)
@@ -23,9 +24,9 @@ fun NavGraphBuilder.addCompanyGraph(
 //                Text("Move To Detail 2")
 //            }
 //        }
-    }
+//    }
 
-    composable(CompanyScreenRouter.DETAIL2.route) {
+//    composable(CompanyScreenRouter.DETAIL2.route) {
 //        DetailScreen(from = CompanyScreenRouter.DETAIL.route + "2")
-    }
+//    }
 }
--- app/src/main/java/com/example/androidcustomui/navigation/nav_graph/HomeNavGraph.kt
+++ app/src/main/java/com/example/androidcustomui/navigation/nav_graph/HomeNavGraph.kt
@@ -5,17 +5,18 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import com.example.androidcustomui.model.screen_router.HomeScreenRouter
+import com.example.androidcustomui.ui.screen.TabComponents
 
 fun NavGraphBuilder.addHomeGraph(
     modifier: Modifier = Modifier,
     navController: NavController
 ) {
     composable(HomeScreenRouter.ROOT.route) {
-//        HomeScreen {
+        TabComponents {
 //            navController.navigate(HomeScreenRouter.DETAIL.route)
-//        }
+        }
     }
-    composable(HomeScreenRouter.DETAIL.route) {
+//    composable(HomeScreenRouter.DETAIL.route) {
 //        DetailScreen(from = HomeScreenRouter.DETAIL.route)
-    }
+//    }
 }

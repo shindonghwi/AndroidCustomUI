ecf0af45106a36163f6bdf7987bd8bf3ff7a63a1
shindonghwi <ehdgnl8940@naver.com>
2022-07-14 15:58:18 +0900

    feat: 컴포넌트, 커스텀뷰 화면 구현

--- app/src/main/java/com/example/androidcustomui/ui/screen/TabComponents.kt
+++ app/src/main/java/com/example/androidcustomui/ui/screen/TabComponents.kt
@@ -1,31 +1,15 @@
 package com.example.androidcustomui.ui.screen
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color.Companion.White
-import androidx.compose.ui.text.style.TextAlign
+import com.example.androidcustomui.components.common.MenuList
 
 @Composable
 fun TabComponents(
     navigateToDetail: () -> Unit
 ) {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(MaterialTheme.colors.primary)
-    ) {
-        Text(
-            text = "TabComponents",
-            style = MaterialTheme.typography.h1,
-            textAlign = TextAlign.Center,
-            color = White,
-            modifier = Modifier.align(Alignment.Center)
+    MenuList(
+        listOf<String>(
+            "Circle\nProgressBar"
         )
-    }
+    )
 }
